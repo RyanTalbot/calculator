047952135db1edfb9de8d853a30dcd62165e130b
Ryan Talbot <ryan-talbot@outlook.com>
2020-06-15 00:28:18 +0100

    Small tweaks to complement base JS work. JS work includes setting up variables and base functions

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-function add(a, b){
-    return a + b;
-}
+// Grabbing all buttons and display and assigning them into variables
+let display = document.querySelector(".digit-display"),
+  delBtn = document.getElementById("delete-key"),
+  numbers = document.querySelectorAll(".numbers"),
+  operation = document.querySelectorAll(".operator"),
+  decBtn = document.getElementById("decimal"),
+  ansButton = document.getElementById("equals");
 
-function subtract(a, b){
-    return a - b;
-}
+//Base operation functions
+add = (num1, num2) => num1 + num2;
 
-function multiply(a, b){
-    return a * b;
-}
+subtract = (num1, num2) => num1 - num2;
 
-function divide(a, b){
-    return a / b;
-}
+multiply = (num1, num2) => num1 * num2;
 
-function operate(operator, a, b){
-    switch(operate()){
-        case "+":
-            add(a, b);
-            break;
-        case "-":
-            subtract(a, b);
-            break;
-        case "*":
-            multiply(a, b);
-            break;
-        case "/":
-            divide(a,b);
-            break;
-        default:
-            return -1;
-    }
-}
+divide = (num1, num2) => num1 / num2;
 
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Error :(";
+  }
+}
