1d9c901290f4911ef9f6fc7cc2bbfe21e1988a5a
Ryan Talbot <ryan-talbot@outlook.com>
2020-06-15 02:13:42 +0100

    JS completed, room for expansion

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let display = document.querySelector(".digit-display"),
   ansButton = document.getElementById("equals"),
   clearBtn = document.getElementById("clear-key");
 
+// Event listeners
+clearBtn.addEventListener("click", () => (display.value = ""));
+delBtn.addEventListener("click", deleteInput);
+ansButton.addEventListener("click", finalAnswer);
+decBtn.addEventListener("click", decimalInput);
+
 //Base operation functions
 add = (num1, num2) => num1 + num2;
 
@@ -39,10 +45,23 @@ function getGivenValue(e) {
     (e.target.innerHTML === "*" || e.target.innerHTML === "/") &&
     display.value === ""
   ) {
-    console.log("yo");
     return;
   }
   // Assigning retrieved numbers into variable and injecting HTML
+  let input = display.value;
+  let inputFinal = input + e.target.innerHTML;
+  display.value = inputFinal;
+}
+
+// Delete Input
+function deleteInput() {
+  let input = display.value;
+  let inputFinal = input.slice(0, -1);
+  display.value = inputFinal;
+}
+
+// Decimal Input
+function decimalInput(e) {
   let input = display.value;
   let inputFinal = input + e.target.innerHTML;
   display.value = inputFinal;
@@ -58,3 +77,42 @@ numbers.forEach((number) =>
 operation.forEach((operator) =>
   operator.addEventListener("click", (e) => getGivenValue(e))
 );
+
+// Operation Results
+function finalAnswer() {
+  // Declaring variables, also splitting each character
+  let displayData = display.value.split(""),
+    operatorPosition,
+    operation,
+    firstNumber,
+    lastNumber,
+    answer,
+    finalAnswer;
+
+  // Loop to locate the operators used
+  for (let i = 0; i < displayData.length; i++) {
+    if (
+      displayData[i] === "+" ||
+      displayData[i] === "-" ||
+      displayData[i] === "*" ||
+      displayData[i] === "/"
+    ) {
+      // Grabbing the index of the operator used
+      operatorPosition = displayData.indexOf(displayData[i]);
+      // Grabbing the actual operator
+      operation = displayData[i];
+      // Extracting first number and placing it in front of the operator
+      firstNumber = parseFloat(displayData.slice(0, operatorPosition).join(""));
+      // Extracting second number and placing it after the operator
+      lastNumber = parseFloat(displayData.slice(operatorPosition + 1).join(""));
+    }
+  }
+
+  // Storing answer into corresponding variable
+  answer = operate(operation, firstNumber, lastNumber);
+  // Moving answer into new final variable
+  finalAnswer = answer;
+  // Displaying answer on screen
+  display.value = finalAnswer;
+  return answer;
+}
