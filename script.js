08cdc74bf7cf2b0aa2779bccc44ebe6eadaecaf5
Ryan Talbot <ryan-talbot@outlook.com>
2020-06-15 01:24:50 +0100

    Added functions for retrieving values and event listeners for numbers and operators

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let display = document.querySelector(".digit-display"),
   numbers = document.querySelectorAll(".numbers"),
   operation = document.querySelectorAll(".operator"),
   decBtn = document.getElementById("decimal"),
-  ansButton = document.getElementById("equals");
+  ansButton = document.getElementById("equals"),
+  clearBtn = document.getElementById("clear-key");
 
 //Base operation functions
 add = (num1, num2) => num1 + num2;
@@ -29,3 +30,31 @@ function operate(operator, num1, num2) {
       return "Error :(";
   }
 }
+
+// Function to extract value provided along with validation.
+function getGivenValue(e) {
+  // User can still make a number positive or negative, but cannot
+  // put + or / in front of numbers unless performing an operation.
+  if (
+    (e.target.innerHTML === "*" || e.target.innerHTML === "/") &&
+    display.value === ""
+  ) {
+    console.log("yo");
+    return;
+  }
+  // Assigning retrieved numbers into variable and injecting HTML
+  let input = display.value;
+  let inputFinal = input + e.target.innerHTML;
+  display.value = inputFinal;
+}
+
+// Adding an event listener for each number. Any number clicked, the info will be sent
+// to getGivenValue function and processed.
+numbers.forEach((number) =>
+  number.addEventListener("click", (e) => getGivenValue(e))
+);
+
+// Same as above, adding an event listener to each operator and extracting the value
+operation.forEach((operator) =>
+  operator.addEventListener("click", (e) => getGivenValue(e))
+);
